1af4ebf1615b946e5256dd40b0f04a01f6271625
Diego Capassi <diego.capassi.moreira@gmail.com>
2020-02-04 00:09:38 -0200

    Created SessionController with JWT

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,36 @@
+//index
+//show
+//store
+//update
+//delete
+
+import User from "../models/User";
+import jwt from "jsonwebtoken";
+import authConfig from "../../configs/authConfig";
+
+class SessionController {
+  async store(req, res) {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      return res.status(401).json({ error: "User not found" });
+    }
+
+    if (!(await user.checkPassword(password))) {
+      return res.status(401).json({ error: "Password does not match" });
+    }
+
+    const { id, name } = user;
+
+    return res.json({
+      user: { id, name, email },
+      token: jwt.sign({ id }, authConfig.secret, {
+        expiresIn: authConfig.expiresIn
+      })
+    });
+  }
+}
+
+export default new SessionController();
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import User from "./app/models/User";
 import UserController from "./app/controllers/UserController";
+import sessionController from "./app/controllers/SessionController";
+import SessionController from "./app/controllers/SessionController";
 
 const routes = new Router();
 
 routes.post("/users", UserController.store);
+routes.post("/sessions", SessionController.store);
 
 export default routes;
